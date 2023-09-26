31f5fd73921db0c3a8135bf74ef3dc32801b25a6
Jakir Hossain <jakirseu@gmail.com>
2023-09-27 05:56:34 +0600

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Jakir Hossain on 27/9/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
